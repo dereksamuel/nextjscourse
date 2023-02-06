a027a0a2f6ce6120c3fde7fe5f7eb614c757c652
Derek Paul <11dereksamuel@gmail.com>
2023-02-06 12:14:35 -0500

    done eslint config fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="bg-red-700">
       <p className="text-white">Hola</p>
+      <p className="text-blue-100">Hola</p>
     </div>
   )
 }
