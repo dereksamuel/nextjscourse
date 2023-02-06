c62e5c23b319cafc2e5f803b1755cf031695e332
Derek Paul <11dereksamuel@gmail.com>
2023-02-06 14:43:39 -0500

    done tailwind course

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <div className="bg-red-700">
-      <p className="text-white">Hola</p>
-      <p className="sky">Hola</p>
+    <div className="bg-light">
+      <button className="bg-primary text-light px-5 py-3">Btn</button>
+      <button className="bg-secondary text-light px-5 py-3">Btn</button>
+      <p className="text-secondary">
+        <strong>Hola</strong>
+      </p>
+      <input placeholder="Hoola" />
+      <p className="text-blue-100">Hola</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: colors.emerald[500],
+      secondary: colors.slate[500],
+      dark: colors.slate[700],
+      light: colors.emerald[50],
+      white: colors.white,
+    },
+    fontFamily: {
+      Nunito: ["Nunito", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 }
